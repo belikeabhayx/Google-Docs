85418f1d336f9829f1f202f29bc72605a3a37180
Abhay Chauhan <chauhanabhay.chauhan2@gmail.com>
2026-06-02 16:07:29 +0530

    feat: add a basic toolbar

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,4 +1,5 @@
 import Editor from "./editor";
+import { Toolbar } from "./toolbar";
 
 interface DocumentIdProps {
   params: Promise<{ documentId: string }>;
@@ -8,6 +9,7 @@ const page = async ({ params }: DocumentIdProps) => {
   const { documentId } = await params;
   return (
     <div className="min-h-screen bg-[#FAFBFD]">
+      <Toolbar />
       <Editor />
     </div>
   );
