66dc5ced966b99e0d3c2a9a80f6e96a752690e39
Abhay Chauhan <chauhanabhay.chauhan2@gmail.com>
2026-06-05 00:37:52 +0530

    feat: integrate Clerk for authentication and add FullscreenLoader component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useQuery } from "convex/react";
 import { Navbar } from "./navbar";
 import TemplateGallery from "./templates/templates-gallery";
@@ -11,6 +12,7 @@ const Home = () => {
       <p>Loading...</p>
     )
   }
+  
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white">
